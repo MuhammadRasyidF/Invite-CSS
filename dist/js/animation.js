2bf74f434cae765977ffdb333ead369302721ce6
revbase28 <reihanreinaldimctna@gmail.com>
2022-12-06 20:16:08 +0700

    Gallery model at example2

--- dist/js/animation.js
+++ dist/js/animation.js
@@ -1,30 +1,17 @@
-<<<<<<< HEAD
-$(window).scroll(function () {
-  var scroll = $(".inv-animate-fade").scrollTop();
-  console.log(scroll);
-  if (scroll >= 100) {
-    $(".inv-animate-fade").fadeOut(2000);
-  } else {
-    $(".inv-animate-fade").fadeIn(2000);
-  }
-});
-=======
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-  
-  window.addEventListener("scroll", reveal);
-  
->>>>>>> c8cdb58f23119d4c599236a0d7eab7734c346544
+}
+
+window.addEventListener("scroll", reveal);
